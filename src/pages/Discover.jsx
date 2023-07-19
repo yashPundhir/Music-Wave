a1d228b277b6142e7b0e3dbadd0dc35d97852d31
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-19 23:56:30 +0530

    Discover page updated

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable import/order */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable indent */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable no-tabs */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+import { useGetTopSongsQuery } from "../redux/services/spotify";
+import { retry } from "@reduxjs/toolkit/dist/query";
+
+const Discover = () => {
+	const { data, isFetching, error } = useGetTopSongsQuery();
+
+	const genreTitle = "Pop";
+
+	const reqData = data && data.slice(0, 20);
+
+	if (isFetching) return <Loader title="Loading Songs..." />;
+
+	if (error) return <Error />;
+
+	return (
+		<div className="flex flex-col">
+			<div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+				<h2 className="font-bold text-3xl text-white text-left">
+					Discover {genreTitle}
+				</h2>
+				<select
+					onChange={() => {}}
+					value=""
+					className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+				>
+					{genres.map((genre) => (
+						<option key={genre.value} value={genre.value}>
+							{genre.title}
+						</option>
+					))}
+				</select>
+			</div>
+			<div className="flex flex-wrap sm:justify-start justify-center gap-8">
+				{reqData?.map((song, i) => (
+					<SongCard key={song.key} song={song} i={i} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Discover;
